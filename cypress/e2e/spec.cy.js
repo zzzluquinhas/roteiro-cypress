0ec8c06597a912e500a367520260bf44a7bbd7e7
Lucas Almeida <72702201+zzzluquinhas@users.noreply.github.com>
2024-06-27 19:41:35 -0300

    adicionados três novos testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -56,4 +56,55 @@ describe('template spec', () => {
     cy.get('.todo-list li')
       .should('have.length', 2);
   });
+
+  it('Edita uma tarefa existente', () => {
+	cy.visit('http://127.0.0.1:7001'); 
+
+	cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+	
+	cy.get('.todo-list li')
+	  .first()
+	  .dblclick();
+
+	cy.get('.todo-list li .edit')
+	  .type('{backspace}{backspace}Engenharia de Software{enter}');
+
+	cy.get('.todo-list li')
+	  .should('have.length', 2)
+	  .first()
+	  .should('have.text', 'TP2 de Engenharia de Software');
+  });
+
+  it('Marca todas as tarefas como completas', () => {
+	cy.visit('http://127.0.0.1:7001'); 
+
+	cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}')
+	  .type('RP2 de ES{enter}');
+
+	cy.get('.toggle-all-label').click();
+	
+	cy.contains('Completed').click();
+    cy.get('.todo-list li')
+      .should('have.length', 3);
+  });
+
+  it('Deleta todas as tarefas completas', () => {
+	cy.visit('http://127.0.0.1:7001'); 
+	
+	cy.get('.new-todo')
+	  .type('TP2 de ES{enter}')
+	  .type('Prova de ES{enter}')
+	  .type('RP2 de ES{enter}');
+
+	cy.get('.toggle-all-label').click();
+
+	cy.get('.clear-completed').click();
+
+	cy.get('.todo-list li')
+	  .should('have.length', 0);
+  });
 });
